0a3d8562b2e35a0295dbbc27a994f94bfb2836cc
“philippe <philippe.mail.perso@gmail.com>
2023-09-30 11:55:30 +0200

    Fix homeonly check on static home page

--- _public.php
+++ _public.php
@@ -23,8 +23,8 @@ class mrvbNextDoorPublic
         if ($w->offline) {
             return;
         }
-        if (($w->homeonly == 1 && dcCore::app()->url->type != 'default') || ($w->homeonly == 2 && dcCore::app()->url->type == 'default')) {
-            return;
+        if (!$w->checkHomeOnly(dcCore::app()->url->type)) {
+            return '';
         }
 
         $res = ($w->title ? $w->renderTitle(html::escapeHTML($w->title)) : '');
